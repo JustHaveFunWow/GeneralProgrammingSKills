10eb2d2eed525e16607b9072eca14f5f588b8be7
Knight-ZXW <nimdanoob@163.com>
2017-09-07 22:29:21 +0800

    add insertion

--- 算法/算法-第四章书/src/chapter2/Insertion.java
+++ 算法/算法-第四章书/src/chapter2/Insertion.java
@@ -7,8 +7,15 @@ import base.StdOut;
  */
 public class Insertion {
     //类似插牌，对于大部分有序序列有优势
+    //如果是反序的序列，则在每次比较中都会交换位置，
     public static void sort(Comparable[] a){
+        for (int i = 1; i < a.length; i++) {
+            //把a[i]插入到 0...i-1中
 
+            for (int j = i; j >0&&less(a[j],a[j-1]) ; j--) {
+                exch(a,j,j-1);
+            }
+        }
     }
 
     private static boolean less(Comparable v, Comparable w){
