44b3d7cef57464e40b2359f0487d775af655dac9
Knight-ZXW <nimdanoob@163.com>
2017-09-12 00:36:35 +0800

    创建图的表现形式

---
+++ 算法/算法-第四章书/src/chapter4/Graph.java
@@ -0,0 +1,39 @@
+package chapter4;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by zhuoxiuwu on 2017/9/12.
+ */
+public class Graph {
+    private int verticals;//顶点个数
+    private int edges;//边数
+    private ArrayList<ArrayList<Integer>> adjacency;//顶点连接列表
+
+    public Graph(int vertical){
+        this.verticals = vertical;
+        this.edges = 0;
+        //也可以用数组表示
+        adjacency = new ArrayList<>();
+        for (int i = 0; i < vertical; i++) {
+            adjacency.set(i, new ArrayList<>());
+        }
+    }
+    public int getVerticals(){
+        return verticals;
+    }
+
+    public int getEdges(){
+        return edges;
+    }
+    public void addEdge(int verStart,int verEnd){
+        adjacency.get(verStart).add(verEnd);
+        adjacency.get(verEnd).add(verStart);
+    }
+
+    public List<Integer> getAdjacency(int vertical){
+        return adjacency.get(vertical);
+    }
+}
