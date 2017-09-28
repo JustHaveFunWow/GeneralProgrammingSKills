4967b7881dae542537a54a047cbd586d66a25655
Knight-ZXW <nimdanoob@163.com>
2017-09-28 14:33:45 +0800

    non decreasing array

--- 算法/leetcode/src/easy/Hamiing_Distance_461.java
+++ 算法/leetcode/src/easy/Hamiing_Distance_461.java
@@ -1,7 +1,25 @@
 package easy;
 
 /**
- * Created by zhuoxiuwu on 2017/9/23.
+ *The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+ Given two integers x and y, calculate the Hamming distance.
+
+ Note:
+ 0 ≤ x, y < 231.
+
+ Example:
+
+ Input: x = 1, y = 4
+
+ Output: 2
+
+ Explanation:
+ 1   (0 0 0 1)
+ 4   (0 1 0 0)
+ ↑   ↑
+
+ The above arrows point to positions where the corresponding bits are different.
  */
 public class Hamiing_Distance_461 {
     public int hammingDistance(int x, int y) {
--- 算法/leetcode/src/easy/Keyboard_Row_500.java
+++ 算法/leetcode/src/easy/Keyboard_Row_500.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- * Created by zhuoxiuwu on 2017/9/27.
+ * Given a List of words, return the words that can be typed using letters of alphabet on only one row's of American keyboard like the image below..
+ Example 1:
+ Input: ["Hello", "Alaska", "Dad", "Peace"]
+ Output: ["Alaska", "Dad"]
+ Note:
+ You may use one character in the keyboard more than once.
+ You may assume the input string will only contain letters of alphabet.
  */
 public class Keyboard_Row_500 {
     public static String[] findWords(String[] words){
