728de07b3031b8e29aedb1619407a7347405b5b7
Knight-ZXW <nimdanoob@163.com>
2018-03-19 20:42:53 +0800

    spring boot hello world

--- project/knight/src/main/java/nimdanoob/knight/KnightApplication.java
+++ project/knight/src/main/java/nimdanoob/knight/KnightApplication.java
@@ -2,8 +2,10 @@ package nimdanoob.knight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 没有配置数据库 暂时，exclude autoconfig
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class KnightApplication {
 
 	public static void main(String[] args) {
