e3f98f57ac3056c44363ca7f2633b7e41f3a21df
Knight-ZXW <nimdanoob@163.com>
2018-03-20 15:19:04 +0800

    添加swagger2

---
+++ project/knight/src/main/java/nimdanoob/knight/Swagger2.java
@@ -0,0 +1,37 @@
+package nimdanoob.knight;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("nimdanoob.knight.web"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("RESTful APIs 文档")
+                .description("使用Swagger2 自动生成的接口文档")
+                .termsOfServiceUrl("http://127.0.0.1:8080/")
+                .contact("nimdanoob")
+                .version("0.1")
+                .build();
+    }
+
+
+}
--- project/knight/src/test/java/nimdanoob/knight/KnightApplicationTests.java
+++ project/knight/src/test/java/nimdanoob/knight/KnightApplicationTests.java
@@ -5,38 +5,59 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import static org.hamcrest.Matchers.equalTo;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class KnightApplicationTests {
-	private MockMvc mvc;
+    private MockMvc mvc;
 
-	@Before
-	public void setUp() throws Exception{
-		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-	}
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
 
-	@Test
-	public void getHello() throws Exception{
-		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string("Hello World"));
-	}
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testUserController() throws Exception {
+        RequestBuilder request = null;
+        request = get("users");
+        //get 查一下user列表，结果应该为空
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("[]"));
+
+        //post 提交一个User
+        request = post("/users/")
+                .param("id","1")
+                .param("name","test master")
+                .param("age","20");
+        mvc.perform(request)
+                .andExpect(content().string("success"));
+
+        //get 获取 user列表
+
+    }
+
+
+
+    @Test
+    public void contextLoads() {
+    }
 
 }
