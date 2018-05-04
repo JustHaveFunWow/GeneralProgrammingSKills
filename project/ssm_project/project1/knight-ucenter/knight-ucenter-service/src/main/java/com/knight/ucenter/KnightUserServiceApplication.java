cf2d32ccbea98e27cc59a9baed9624ad6dd573fe
Knight-ZXW <nimdanoob@163.com>
2018-05-04 15:27:40 +0800

    mybatis-spring-boot optional

--- project/ssm_project/project1/knight-ucenter/knight-ucenter-service/src/main/java/com/knight/ucenter/KnightUserServiceApplication.java
+++ project/ssm_project/project1/knight-ucenter/knight-ucenter-service/src/main/java/com/knight/ucenter/KnightUserServiceApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 
